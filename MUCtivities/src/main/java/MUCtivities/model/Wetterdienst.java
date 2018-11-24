d36d6e270b929434f03bb7e0fe35a5e91d42f10c
Andreas Hartung <Andreas@ANDREAS>
2018-11-24 14:49:10 +0100

    Alexa: more specific definition of good-weather
    Issues: #21

--- MUCtivities/src/main/java/MUCtivities/model/Wetterdienst.java
+++ MUCtivities/src/main/java/MUCtivities/model/Wetterdienst.java
@@ -15,12 +15,13 @@ import org.json.JSONObject;
 public class Wetterdienst {
     
     private static final String api = "https://api.openweathermap.org/data/2.5/weather?q=Munich&APPID=c5a122073fa1a5d290b62089e179531b";
+    private static final int KELVIN_CELSIUS_DIFFERENCE = 273;
     
     public static Optional<Boolean> wetterVorhersage() {
         Boolean result;
         try {
             HttpsURLConnection apiConnection = callApi();
-            String responsebody = getResponsebody(apiConnection);
+            JSONObject responsebody = getResponsebody(apiConnection);
             result = isGoodWeather(responsebody);            
         } catch (Exception e) {
             result = null;
@@ -28,26 +29,44 @@ public class Wetterdienst {
         return Optional.ofNullable(result);
     }
 
-    static Boolean isGoodWeather(String responsebody) {
-        String weatherIcon = getWeatherIcon(responsebody);
+    static Boolean isGoodWeather(JSONObject responseBody) {
+    	int weatherIcon = getWeatherIcon(responseBody);
+    	int temperature = getTemperature(responseBody);
+        return weatherIcon < 3 && temperature >= 15;
+    }
+
+    /**
+     * Reads the Kelvin temperature and converts in to Celsius.
+     * @param responseBody
+     * @return Temperature in Celsius
+     */
+	static int getTemperature(JSONObject responseBody) {
+		int kelvinTemp = responseBody.getJSONObject("main").getInt("temp");
+		return kelvinTemp - KELVIN_CELSIUS_DIFFERENCE;
+	}
+
+	/**
+     * First two chars of weather icon
+     * Weather Icon format in responseBody is: ddx
+     * "d" represents a decimal and "x" is either character 'd' (day) or 'n' (night)
+     * @param responseBody Response as json
+     * @return Returns weather icon as decimal consisting of the first two characters
+     */
+    static Integer getWeatherIcon(JSONObject responseBody) {
+        String weatherIcon = responseBody.getJSONArray("weather").getJSONObject(0).getString("icon");
         String FirstTwoCharsOfWeatherIcon = weatherIcon.substring(0, 2);
-        int weather = new Integer(FirstTwoCharsOfWeatherIcon);
-        return weather < 3;
+    	return new Integer(FirstTwoCharsOfWeatherIcon);
     }
 
-    static String getWeatherIcon(String responsebody) {
-        JSONObject json = new JSONObject(responsebody);
-        return json.getJSONArray("weather").getJSONObject(0).getString("icon");
-    }
-
-    static String getResponsebody(HttpsURLConnection apiConnection) throws IOException {
+    static JSONObject getResponsebody(HttpsURLConnection apiConnection) throws IOException {
         InputStream in = apiConnection.getInputStream();
         ByteArrayOutputStream buffer = new ByteArrayOutputStream();
         int byteBlock;
         while ((byteBlock = in.read()) != -1) {
             buffer.write(byteBlock);
         }
-        return buffer.toString();
+        String responseBody = buffer.toString();
+        return new JSONObject(responseBody);
     }
 
     static HttpsURLConnection callApi() throws MalformedURLException, IOException, ProtocolException {
--- MUCtivities/src/test/java/MUCtivities/model/WetterdienstTest.java
+++ MUCtivities/src/test/java/MUCtivities/model/WetterdienstTest.java
@@ -2,6 +2,7 @@ package MUCtivities.model;
 
 
 
+import org.json.JSONObject;
 import org.junit.Assert;
 
 import java.util.Optional;
@@ -15,24 +16,28 @@ import org.junit.Test;
 public class WetterdienstTest {
 
     String responseExampleBadWeather = "{\"coord\":{\"lon\":11.58,\"lat\":48.14},\"weather\":[{\"id\":701,\"main\":\"Mist\",\"description\":\"mist\",\"icon\":\"50n\"},{\"id\":300,\"main\":\"Drizzle\",\"description\":\"light intensity drizzle\",\"icon\":\"09n\"},{\"id\":741,\"main\":\"Fog\",\"description\":\"fog\",\"icon\":\"50n\"}],\"base\":\"stations\",\"main\":{\"temp\":273.29,\"pressure\":1016,\"humidity\":100,\"temp_min\":271.15,\"temp_max\":275.15},\"visibility\":3500,\"wind\":{\"speed\":2.1,\"deg\":120},\"clouds\":{\"all\":90},\"dt\":1542822600,\"sys\":{\"type\":1,\"id\":4914,\"message\":0.0271,\"country\":\"DE\",\"sunrise\":1542781808,\"sunset\":1542814129},\"id\":2867714,\"name\":\"Munich\",\"cod\":200}";
-    String responseExampleGoodWeather = "{\"coord\":{\"lon\":11.58,\"lat\":48.14},\"weather\":[{\"id\":701,\"main\":\"Mist\",\"description\":\"mist\",\"icon\":\"01n\"},{\"id\":300,\"main\":\"Drizzle\",\"description\":\"light intensity drizzle\",\"icon\":\"09n\"},{\"id\":741,\"main\":\"Fog\",\"description\":\"fog\",\"icon\":\"50n\"}],\"base\":\"stations\",\"main\":{\"temp\":273.29,\"pressure\":1016,\"humidity\":100,\"temp_min\":271.15,\"temp_max\":275.15},\"visibility\":3500,\"wind\":{\"speed\":2.1,\"deg\":120},\"clouds\":{\"all\":90},\"dt\":1542822600,\"sys\":{\"type\":1,\"id\":4914,\"message\":0.0271,\"country\":\"DE\",\"sunrise\":1542781808,\"sunset\":1542814129},\"id\":2867714,\"name\":\"Munich\",\"cod\":200}";
+    String responseExampleGoodWeather = "{\"coord\":{\"lon\":11.58,\"lat\":48.14},\"weather\":[{\"id\":701,\"main\":\"Mist\",\"description\":\"mist\",\"icon\":\"01n\"},{\"id\":300,\"main\":\"Drizzle\",\"description\":\"light intensity drizzle\",\"icon\":\"09n\"},{\"id\":741,\"main\":\"Fog\",\"description\":\"fog\",\"icon\":\"50n\"}],\"base\":\"stations\",\"main\":{\"temp\":293.29,\"pressure\":1016,\"humidity\":100,\"temp_min\":271.15,\"temp_max\":275.15},\"visibility\":3500,\"wind\":{\"speed\":2.1,\"deg\":120},\"clouds\":{\"all\":90},\"dt\":1542822600,\"sys\":{\"type\":1,\"id\":4914,\"message\":0.0271,\"country\":\"DE\",\"sunrise\":1542781808,\"sunset\":1542814129},\"id\":2867714,\"name\":\"Munich\",\"cod\":200}";
 
     @Test
-    public void testMain() {
+    public void testWettervorhersage() {
         Optional<Boolean> isGoodWeather = Wetterdienst.wetterVorhersage();
         Assert.assertTrue(isGoodWeather.isPresent());
     }
     
     @Test
     public void testGetWeatherIcon() {
-        String weatherIcon = Wetterdienst.getWeatherIcon(responseExampleGoodWeather);
-        Assert.assertEquals(3, weatherIcon.length());
+    	JSONObject json = new JSONObject(responseExampleGoodWeather);
+        int weatherIcon = Wetterdienst.getWeatherIcon(json);
+        boolean weatherIconIsOnlyTwoDigitsLong = weatherIcon < 100;
+        Assert.assertTrue(weatherIconIsOnlyTwoDigitsLong);
     }
 
     @Test
     public void testIsGoodWeather() {
-        Assert.assertEquals(false, Wetterdienst.isGoodWeather(responseExampleBadWeather));
-        Assert.assertEquals(true, Wetterdienst.isGoodWeather(responseExampleGoodWeather));
+    	JSONObject jsonBad = new JSONObject(responseExampleBadWeather);
+    	JSONObject jsonGood = new JSONObject(responseExampleGoodWeather);
+        Assert.assertEquals(false, Wetterdienst.isGoodWeather(jsonBad));
+        Assert.assertEquals(true, Wetterdienst.isGoodWeather(jsonGood));
     }
 
 }
