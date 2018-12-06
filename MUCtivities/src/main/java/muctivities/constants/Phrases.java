041bd8a40a96ce2eaaa5dfc33de0b2ce3716934e
jwalter <jwalter@hm.edu>
2018-12-06 21:52:02 +0100

    Fixed spelling mistake
    Fixed

--- MUCtivities/src/main/java/muctivities/constants/Phrases.java
+++ MUCtivities/src/main/java/muctivities/constants/Phrases.java
@@ -7,7 +7,7 @@ public class Phrases {
 
 	public static final String MUCTIVITIES_NAME = "MUCtivities";
 
-	public static final String LAUNCH_REQUEST = "Servus! Ein schöner Tag, um mal wieder etwas zu unternehmen. Sage \"Tipp des Tages\" um eine Vorschlag für eine beliebige Aktivität zu erhalten. Für eine personalisierte Idee, sage \"Was kann ich heute unternehmen?\"";
+	public static final String LAUNCH_REQUEST = "Servus! Ein schöner Tag, um mal wieder etwas zu unternehmen. Sage \"Tipp des Tages\" um einen Vorschlag für eine beliebige Aktivität zu erhalten. Für eine personalisierte Idee, sage \"Was kann ich heute unternehmen?\"";
 	public static final String WELCOME_REPROMT = "Hallo Schlaffmütze";
 
 	public static final String CANCEL_AND_STOP_INTENT = "Komm morgen wieder, um die Erfahrung wieder zu spielen. Sag einfach \"Alexa, öffne Muctivities\" <break time=\"0.05s\" />, um die Fertigkeit erneut zu öffnen.";
