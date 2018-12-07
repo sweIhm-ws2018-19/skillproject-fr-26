f8bcc709e74d3884a7b20ca58f2362f9e7046b6b
SPortugall <s.portugall@hm.edu>
2018-12-07 08:44:57 +0100

    Spaß Spass

--- MUCtivities/src/main/java/muctivities/model/Kategorie.java
+++ MUCtivities/src/main/java/muctivities/model/Kategorie.java
@@ -1,21 +1,24 @@
 package muctivities.model;
 
 public enum Kategorie {
-	KULTUR, SPASS, SPORT;
+    KULTUR, SPASS, SPORT;
 
-	/**
-	 * Parses a string to a enum category. Prefer this instead of valueOf() to
-	 * ignore the case of the string parameter.
-	 * 
-	 * @param categoryName Name of the category
-	 * @return Category matching the given name. No-match returns null.
-	 */
-	public static Kategorie parseString(String categoryName) {
-		for (Kategorie kategorie : Kategorie.class.getEnumConstants()) {
-			if (kategorie.name().equalsIgnoreCase(categoryName)) {
-				return kategorie;
-			}
-		}
-		return null;
-	}
+    /**
+     * Parses a string to a enum category. Prefer this instead of valueOf() to
+     * ignore the case of the string parameter.
+     *
+     * @param categoryName Name of the category
+     * @return Category matching the given name. No-match returns null.
+     */
+    public static Kategorie parseString(String categoryName) {
+        for (Kategorie kategorie : Kategorie.class.getEnumConstants()) {
+            if (kategorie.name().equalsIgnoreCase(categoryName)) {
+                return kategorie;
+            }
+            if (kategorie.name().equalsIgnoreCase("SPASS") && categoryName.equalsIgnoreCase("Spaß")) {
+                return kategorie;
+            }
+        }
+        return null;
+    }
 }
